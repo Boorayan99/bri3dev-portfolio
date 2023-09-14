0b7d52059ae44ad6f51064054f5bd8623d320777
Boorayan99 <brianoumo505@gmail.com>
2023-09-15 00:00:26 +0300

    Content Update_01

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -78,13 +78,11 @@ const Works = () => {
       <div className='w-full flex'>
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          className='w-full flex mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          Each project is briefly described with links to code repositories and 
+          live demos in it showing my ability to solve complex problems, work with
+          different technologies, and manage projects effectively.
         </motion.p>
       </div>
 
