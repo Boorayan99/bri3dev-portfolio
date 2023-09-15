de4e3c8558347bffa79ead4129531ad60b007d42
Boorayan99 <brianoumo505@gmail.com>
2023-09-15 13:34:53 +0300

    Navbar Fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>Boorayan&nbsp;
-            <span className='sm:block hidden'>| Portfolio </span>
+            <span className='sm:block hidden'>| Developer Portfolio </span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
